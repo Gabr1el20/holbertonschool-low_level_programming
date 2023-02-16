2787feb5d809d56124c5eb91751f4c5edbe19e49
Gabr1el20 <6327@holbertonstudents.com>
2023-02-16 06:20:10 -0800

    Numbers

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ *	main - Entry point
+ *
+ *	Return: Always 0 (success)
+ *
+ */
+
+	int main(void)
+{
+	int numero;
+
+	for (numero = 0; numero < 10; numero++)
+		printf("%d", numero);
+	printf("\n");
+	return (0);
+}
