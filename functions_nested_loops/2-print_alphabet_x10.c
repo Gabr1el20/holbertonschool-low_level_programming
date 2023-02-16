8493348ecf0d7743d648ab713586881485941d88
Gabr1el20 <6327@holbertonstudents.com>
2023-02-16 11:44:39 -0800

    10x alphabet

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,25 @@
+#include <unistd.h>
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints the alphabet ten times
+ *
+ *  Return: void
+ */
+	void print_alphabet_x10(void)
+
+{
+	char x;
+	int q = 0;
+
+	while (q < 9)
+	{
+
+	for (x = 'a'; x <= 'z'; x++)
+{
+	_putchar(x);
+}
+	_putchar('\n');
+	q++;
+}
+}
