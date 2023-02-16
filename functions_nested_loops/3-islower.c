6724f092e43d7b7d59b6e53d07d47f870f5caef0
Gabr1el20 <6327@holbertonstudents.com>
2023-02-16 12:20:48 -0800

    islower

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,17 @@
+#include <unistd.h>
+#include "main.h"
+
+/**
+ * _islower - Check if there exist a lowercase
+ * @c: the character to be checked
+ *
+ * return 1 for lower character and 0 for anything else
+ */
+	int _islower(int c)
+{
+	if (c >= 97 && c <= 122)
+	{
+		return (1);
+	}
+	return (0);
+}
