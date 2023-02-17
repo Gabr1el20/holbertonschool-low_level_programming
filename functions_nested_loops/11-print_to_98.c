4916d246793e5f07ef9c8419dfb91ff70972477e
Gabr1el20 <6327@holbertonstudents.com>
2023-02-17 07:25:14 -0800

    98 Battery Street

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -11,13 +11,13 @@
 	if (n >= 98)
 	{
 		while (n > 98)
-			printf("%d,", n--);
+			printf("%d, ", n--);
 		printf("%d\n", n);
 	}
 	else
 	{
 		while (n < 98)
-			printf("%d,", n++);
+			printf("%d, ", n++);
 		printf("%d\n", n);
 	}
 }
