cd41c5c8f585d78921101331a5178ed1546a6d33
Gabr1el20 <6327@holbertonstudents.com>
2023-02-17 11:37:23 -0800

    Quotev4

--- hello_world/101-quote.c
+++ hello_world/101-quote.c
@@ -1,13 +1,14 @@
  #include <stdio.h>
  #include <unistd.h>
+
  /**
-  * main - Entry point
+  * main - prints the message below.
   *
   * Return: always 1
   */
 	int main(void)
 {
 	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"
-			, 58);
-	return(1);
+			, 59);
+	return (1);
 }
