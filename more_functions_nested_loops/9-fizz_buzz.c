242a4ec5c7e4ed11be690e9ea3103113f49afc32
Gabr1el20 <6327@holbertonstudents.com>
2023-02-23 09:02:11 -0800

    Fizz-Buzz

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+/**
+ * main - entry point.
+ *
+ * Return: Placeholder.
+ */
+int main(void)
+{
+	int x;
+
+	for (x = 1; x <= 100; x++)
+	{
+		if ((x % 3) == 0 && (x % 5) == 0)
+		{
+			printf("FizzBuzz");
+			printf(" ");
+		}
+
+		else if ((x % 5) == 0)
+		{
+			printf("Buzz");
+			printf(" ");
+		}
+
+		else if ((x % 3) == 0)
+		{
+			printf("Fizz");
+			printf(" ");
+		}
+
+		else
+		{
+			printf("%d", x);
+			printf(" ");
+		}
+	}
+	return (0);
+}
