af41fef9188f6518ec5a446a60f20fd516a32b9a
Gabr1el20 <6327@holbertonstudents.com>
2023-02-28 09:06:06 -0800

    Test7

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -8,25 +8,14 @@ void puts_half(char *str)
 {
 	int count, i = 0;
 
-	for (count = 0; str[count] != 0; count++)
+for (count = 0; str[count] != 0; count++)
 
-	i = (count - 1) / 2;
+i = (count - 1) / 2 + 1;
 
-	if ((count % 2) == 0)
+	while (str[i])
 	{
-		while (str[i])
-	{
-			_putchar(str[i + 1]);
+		_putchar(str[i]);
 			i++;
-	}
-	}
-	else if ((count % 2) != 0)
-	{
-		while (str[i])
-		{
-			_putchar(str[i + 2]);
-			i++;
-		}
 	}
 	_putchar('\n');
 	}
