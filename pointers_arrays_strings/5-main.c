97a9fb5295d99b8b83699f75ff0cc34c2b31b05a
Gabr1el20 <6327@holbertonstudents.com>
2023-02-28 05:51:59 -0800

    IDK

--- pointers_arrays_strings/5-main.c
+++ pointers_arrays_strings/5-main.c
@@ -9,9 +9,13 @@
 int main(void)
 {
 	char s[1] = "";
+	char x[11] = "Holberton!";
 
 	printf("%s\n", s);
+	printf("%s\n", x);
 	rev_string(s);
+	rev_string(x);
 	printf("%s\n", s);
+	printf("%s\n", x);
 	return (0);
 }
