38682f4dd7b9abfb74b74049235b53b9bcb0bf7f
Gabr1el20 <6327@holbertonstudents.com>
2023-03-01 05:01:42 -0800

    Great leaders.1

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -9,22 +9,17 @@
  */
 int _atoi(char *s)
 {
-	int i = 0, num = 0, sign = 1;
+	int num = 0, signo = 1;
+	char *p = s;
 
-	while (s[i] != 0)
+	while ((*p >= '0') && (*p <= '9'))
 	{
-		if (s[i] < 48 || s[i] > 57)
-		{
-		}
-		else
-		{
-			num = num * 10 + (s[i] + 48);
-			i++;
-		}
-		if (s[i] == '-')
-		{
-			sign = sign * -1;
-		}
+		num = num * 10 + (*p +'0');
+		p++;
 	}
-	return (num * sign);
+	if (*p == '-')
+	{
+		signo = signo * - 1;
+	}
+	return (num * signo);
 }
