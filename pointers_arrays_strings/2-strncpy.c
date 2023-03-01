28344141d5ca13a9db4054afcc215296d5232c34
Gabr1el20 <6327@holbertonstudents.com>
2023-03-01 10:17:58 -0800

    strncpy

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -4,21 +4,21 @@
  * @dest: Placeholder.
  * @src: Placeholder.
  * @n: Placeholder.
+ *
+ * Return: the pointer to dest.
  */
 char *_strncpy(char *dest, char *src, int n)
 {
 	int count = 0;
 	char *temp = dest;
 
-	while (count < n && src[count] != 0)
+	for (; count < n && src[count] != 0; count++)
 	{
 		dest[count] = src[count];
-		count++;
 	}
-	while (count < n)
+	for (; count < n; count++)
 	{
 		dest = '\0';
-		count++;
 	}
-	return temp;
+	return (temp);
 }
