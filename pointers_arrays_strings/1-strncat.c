ffb9c8b94e3aff5b647ce8ac2ebf47802994367c
Gabr1el20 <6327@holbertonstudents.com>
2023-03-01 07:51:31 -0800

    strncat

---
+++ pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * _strncat - Concatenates two strings
+ * @dest: destinary.
+ * @src: source
+ * @n: the amount of bytes.
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	char *str1 = dest;
+	char *str2 = src;
+	char *temp = str1;
+
+	if (n > 0)
+	{
+		while (*str1)
+		{
+			str1++;
+		}
+
+		while (*str2 && n > 0)
+		{
+			*str1 = *str2;
+			str1++;
+			str2++;
+			n--;
+		}
+	}
+	else
+		dest = '\0';
+	return (temp);
+}
