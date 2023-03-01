f1cbe89bab2a6d9f4705fe6528d565898cbc6810
Gabr1el20 <6327@holbertonstudents.com>
2023-03-01 10:25:27 -0800

    strncPY

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -12,13 +12,15 @@ char *_strncpy(char *dest, char *src, int n)
 	int count = 0;
 	char *temp = dest;
 
-	for (; count < n && src[count] != 0; count++)
+	while (count < n && src[count] != '\0')
 	{
 		dest[count] = src[count];
+		count++;
 	}
-	for (; count < n; count++)
+	while (count < n)
 	{
-		dest = '\0';
+		dest[count] = '\0';
+		count++;
 	}
 	return (temp);
 }
