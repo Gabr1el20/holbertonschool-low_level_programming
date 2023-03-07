1fa712e9f4baf55162d43cc5b31bb973ee98f5c8
Gabr1el20 <6327@holbertonstudents.com>
2023-03-07 04:15:57 -0800

    Your subconscius is looking

---
+++ recursion/5-sqrt_recursion.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * _sqrt_recursion - Returns the nartural square root of a number.
+ * @n: The natural number.
+ *
+ * Return: The square root
+ */
+int aux(int x, int y);
+
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+		return (-1);
+
+	return (aux(n, 1));
+}
+
+/**
+ * aux - Auxiliar function to calculate square roots.
+ * @x: Placeholder.
+ * @y: The calculator.
+ *
+ * Return: Placeholder.
+ */
+int aux(int x, int y)
+{
+	if (y * y > x)
+		return (-1);
+
+	if (y * y == x)
+		return (y);
+
+	return (aux(x, y + 1));
+}
