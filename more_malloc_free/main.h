854314c036cf4cca208b6c335871d1052a569721
Gabr1el20 <6327@holbertonstudents.com>
2023-03-16 13:40:03 -0700

    Clean of more_malloc_free

--- more_malloc_free/main.h
+++ more_malloc_free/main.h
@@ -2,5 +2,8 @@
 #define _MAIN_H_
 
 void *malloc_checked(unsigned int b);
+char *string_nconcat(char *s1, char *s2, unsigned int );
+void *_calloc(unsigned int nmemb, unsigned int size);
+int *array_range(int min, int max);
 
 #endif
