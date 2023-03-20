4f9765b421eec94a2cb5ef9c7816663d7ef9bd58
Gabr1el20 <6327@holbertonstudents.com>
2023-03-20 05:53:04 -0700

    Poppy

---
+++ structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+
+/**
+ * struct dog - Struct named dog
+ * @name: Name of dog.
+ * @owner: Name of owner.
+ * @age: Age of the dog
+ */
+struct dog
+{
+	char *name;
+	char *owner;
+	float age;
+};
+
+#endif
