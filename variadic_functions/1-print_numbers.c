1f5d02999ca5ad8536fcc54703286df1da872da7
Gabr1el20 <6327@holbertonstudents.com>
2023-03-24 06:25:31 -0700

    fix- To be is to be the value of the variable

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -19,7 +19,6 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	{
 		num = va_arg(namba, int);
 		printf("%d", num);
-	
 
 	if (separator != NULL && i < n - 1)
 		printf("%s", separator);
