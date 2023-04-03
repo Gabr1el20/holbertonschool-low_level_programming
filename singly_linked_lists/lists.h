f1fbafd0816561ba089bc6a523d57fd4a18fdd5e
Gabr1el20 <6327@holbertonstudents.com>
2023-04-03 07:57:25 -0700

    Print list

---
+++ singly_linked_lists/lists.h
@@ -0,0 +1,24 @@
+#ifndef _LISTS_H_
+#define _LISTS_H_
+
+#include <stdio.h>
+#include <stddef.h>
+
+/**
+ * struct list_s - Singly linked list
+ * @str: String - (Malloc'ed string).
+ * @len: Length of the string.
+ * @next: Point to the next node
+ *
+ * Description: Singly linked list node structure
+ */
+typedef struct list_s
+{
+	char *str;
+	unsigned int len;
+	struct list_s *next;
+} list_t;
+
+size_t print_list(const list_t *h);
+
+#endif
