c9e6c5a0411fbaf201ebbe6b4b35c5ea1e21b2af
Gabr1el20 <6327@holbertonstudents.com>
2023-04-04 08:15:22 -0700

    Insert at index -wihout return NULL-

---
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,34 @@
+#include "lists.h"
+
+/**
+ * insert_dnodeint_at_index - Inserts a new node
+ * at a given position
+ * @h: Pointer-to-pointer.
+ * @idx: the index.
+ * @n: int n
+ *
+ * Return: The address of the new node.
+ */
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+	dlistint_t *nuevo = NULL;
+	dlistint_t *temp = *h;
+	dlistint_t *temp2 = NULL;
+
+	nuevo = malloc(sizeof(dlistint_t));
+	if (nuevo == NULL)
+		return (NULL);
+	nuevo->n = n;
+
+	while (idx != 1)
+	{
+		temp = temp->next;
+		idx--;
+	}
+	temp2 = temp->next;
+	temp->next = nuevo;
+	temp2->prev = nuevo;
+	nuevo->next = temp2;
+	nuevo->prev = temp;
+	return (temp->next);
+}
