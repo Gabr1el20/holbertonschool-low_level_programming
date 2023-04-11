ddcd4a0d7635b4d90d2b011473c2c3cd8ea4af7d
Gabr1el20 <6327@holbertonstudets.com>
2023-04-11 15:47:52 -0300

    Under the snow -fix betty-

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -1,9 +1,10 @@
 #include "main.h"
 
-/*
+/**
  * create_file - function that creates a file
- * filename: name of the file
- * text_content: NULL terminated string
+ * @filename: name of the file
+ * @text_content: NULL terminated string
+ *
  * Return: 1 on success, -1 on failure
  */
 int create_file(const char *filename, char *text_content)
