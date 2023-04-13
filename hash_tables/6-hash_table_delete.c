d802ceea3c5c39a0f67aa7c07d0212b4372d6d46
Gabr1el20 <6327@holbertonstudents.com>
2023-04-13 11:16:32 -0700

    Delete a hash table -changing method-

--- hash_tables/6-hash_table_delete.c
+++ hash_tables/6-hash_table_delete.c
@@ -1,31 +1,25 @@
 #include "hash_tables.h"
 
 /**
- * hash_node_delete - Deletes a node
- * @nd: node
- */
-void hash_node_delete(hash_node_t *nd)
-{
-	free(nd->key);
-	free(nd->value);
-	free(nd);
-}
-
-/**
  * hash_table_delete - deletes a hash table
  * @ht: the hash table to be deleted
  */
 void hash_table_delete(hash_table_t *ht)
 {
 	unsigned long int i;
-	hash_node_t *node;
+	hash_node_t *actual = NULL;
 
 	for (i = 0; i < ht->size; i++)
 	{
-		node = ht->array[i];
-		if (node != NULL)
-			hash_node_delete(node);
+		actual = ht->array[i];
+		while (actual)
+		{
+			free(actual->value);
+			free(actual->key);
+			free(actual);
+			actual = actual->next;
+		}
 	}
-	free(ht->array);
+	free(ht->array[i]);
 	free(ht);
 }
