73ef84e90d2733028f00324fb6567898bc709f87
Gabr1el20 <6327@holbertonstudents.com>
2023-04-13 07:14:28 -0700

    Retrieves the value of a key

--- hash_tables/4-hash_table_get.c
+++ hash_tables/4-hash_table_get.c
@@ -10,7 +10,7 @@
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
 	unsigned long int index = key_index((const unsigned char *) key, ht->size);
-	hash_node_t *nodoi = ht->array[index];
+	hash_node_t *nodo = ht->array[index];
 	char *save;
 
 	if (!ht || !key)
