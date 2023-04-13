3453182c6326718f32328bf684c65c882016bc2c
Gabr1el20 <6327@holbertonstudents.com>
2023-04-13 06:34:45 -0700

    Add an element to a table -fix key == NULL-

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -13,6 +13,9 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	unsigned long int index;
 	hash_node_t *nuevo, *otro;
 
+	if (!key)
+		return (0);
+
 	index = key_index((const unsigned char *) key, ht->size);
 
 	nuevo = ht->array[index];
